4157c2d538a7275d2ff8abc2f50270d88aa59b08
Lucas <lucasp.b@hotmail.com>
2021-01-06 22:20:46 -0300

    - Implementado o controller 'NegociacaoController.ts'.

---
+++ app/js/controllers/NegociacaoController.js
@@ -0,0 +1,12 @@
+class NegociacaoController {
+    constructor() {
+        this._inputData = document.querySelector('#data');
+        this._inputQuantidade = document.querySelector('#quantidade');
+        this._inputValor = document.querySelector('#valor');
+    }
+    adicionar(event) {
+        event.preventDefault(); // serve para não recarregar a tela ao dar submit
+        const negociacao = new Negociacao(new Date(this._inputData.value.replace(/-/g, ',')), parseInt(this._inputQuantidade.value), parseFloat(this._inputValor.value));
+        console.log(negociacao.quantidade + 20);
+    }
+}
