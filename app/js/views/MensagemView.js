2ca6ff277e98aba1a670e63a6715bc7cdba81fab
Lucas <lucasp.b@hotmail.com>
2021-01-07 22:04:36 -0300

    - Convertido os arquivos para o sistema de módulos ecma script.

--- app/js/views/MensagemView.js
+++ app/js/views/MensagemView.js
@@ -1,4 +1,5 @@
-class MensagemView extends View {
+import { View } from './View';
+export class MensagemView extends View {
     template(mensagem) {
         return `
             <p class="alert-info">${mensagem}</p>
